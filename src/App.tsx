8e8a833f36da600cdabc5e6bedfd5a1bfd9ce4cc
padmaUI <padmauideveloper@gmail.com>
2021-01-27 21:26:54 +0530

    fix the issues

--- src/App.tsx
+++ src/App.tsx
@@ -61,13 +61,13 @@ const store = createStore();
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: 'rgb(39,49,66)',
+      main: 'rgb(35,49,66)',
     },
     secondary: {
-      main: 'rgb(197,208,222)',
+      main: 'rgb(200,208,222)',
     },
     background: {
-      default: 'rgb(226,231,238)',
+      default: 'rgb(220,231,238)',
     },
   },
 });
